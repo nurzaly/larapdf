9ebff4bee4fd43dffcb5e54808da9a1284ee25ed
erosensey <erosensey@gmail.com>
2020-10-01 21:49:18 +0800

    contribition test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravelxxx
                 </div>
 
                 <div class="links">
