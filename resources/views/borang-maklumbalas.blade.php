f85436248ebfbe819e50243014107afb1a8f62fa
mohdkairulnizam <64393569+mohdkairulnizam@users.noreply.github.com>
2020-10-02 23:48:15 +0800

    borang maklumbalas

---
+++ resources/views/borang-maklumbalas.blade.php
@@ -0,0 +1,219 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Borang Pemohonan</title>
+<link rel="stylesheet" href="pdf.css">
+</head>
+
+<body>
+<table width="100%" border="0" cellspacing="0" cellpadding="0"  align="center">
+  <tbody>
+    <tr>
+      <td><table width="100%" border="0" align="center"  cellpadding="0" cellspacing="0">
+        <tbody>
+          <tr>
+            <td style="text-align: right">BRG/KRTRS - MAKLUMBALAS PEMOHON BERJAYA</td>
+          </tr>
+          <tr>
+            <td style="text-align: center"><table width="100%" border="0" cellspacing="0" cellpadding="5">
+              <tbody>
+                <tr>
+                  <td align="center" class="bg border" style="border: 1px solid black">BORANG MAKLUMBALAS PEMOHON BERJAYA</td>
+                </tr>
+              </tbody>
+            </table></td>
+          </tr>
+          <tr>
+            <td style="text-align: left;">&nbsp;</td>
+          </tr>
+          <tr>
+            <td style="text-align: left;"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tbody>
+                <tr>
+                  <td width="21%">Nama Pemohon :</td>
+                  <td width="79%" class="underline tdborder">&nbsp;</td>
+                </tr>
+              </tbody>
+            </table></td>
+          </tr>
+          <tr>
+            <td style="text-align: right">&nbsp;</td>
+          </tr>
+          <tr>
+            <td style="text-align: left"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tbody>
+                <tr>
+                  <td width="32%">No. Rujukan Surat Keputusan :</td>
+                  <td width="68%" class="underline tdborder">&nbsp;</td>
+                </tr>
+              </tbody>
+            </table></td>
+          </tr>
+          <tr>
+            <td style="text-align: right">&nbsp;</td>
+          </tr>
+          <tr>
+            <td style="text-align: left"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tbody>
+                <tr>
+                  <td width="29%">No. Rujukan Permohonan :</td>
+                  <td width="71%" class="underline tdborder">&nbsp;</td>
+                </tr>
+              </tbody>
+            </table></td>
+          </tr>
+          <tr>
+            <td style="text-align: right">&nbsp;</td>
+          </tr>
+          <tr>
+            <td>Saya dengan ini mengesahkan bahawa saya;</td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td width="20%">&nbsp;</td>
+                <td width="80%"><strong>BERSETUJU</strong> <span>dan terima tawaran untuk menduduki kuarters bernombor</span></td>
+              </tr>
+              <tr>
+                <td>&nbsp;</td>
+                <td>F2-A1</td>
+              </tr>
+              <tbody>
+                </tbody>
+            </table></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td width="20%">&nbsp;</td>
+                <td width="80%"><strong>TIADA LAGI KEPERLUAN</strong> untuk menduduki kuarters pada masa ini dan</td>
+              </tr>
+              <tr>
+                <td>&nbsp;</td>
+                <td>faham bahawa saya perlu mengemukakan permohonan baharu sekiranya</td>
+              </tr>
+              <tr>
+                <td>&nbsp;</td>
+                <td>ingin memohon menduduki kuarters pada masa hadapan.</td>
+              </tr>
+              <tbody>
+              </tbody>
+            </table></td>
+          </tr>
+          <tr>
+            <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tbody>
+                <tr>
+                  <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tbody>
+                      <tr>
+                        <td>&nbsp;</td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                      </tr>
+                      <tr>
+                         <td style="font-weight: normal; text-decoration: underline; text-align: left;"><p>Diisi sekiranya bersetuju untuk menduduki kuarters:</p></td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                      </tr>
+                      <tr>
+                        <td>Saya turut mengesahkan bahawa saya akan mula menduduki kuarters pada;</td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                      </tr>
+                      <tr>
+                        <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                          <tbody>
+                            <tr>
+                              <td width="33%" style="text-align: center">No. Rujukan Permohonan :</td>
+                              <td width="67%" class="tdborder"></td>
+                            </tr>
+                          </tbody>
+                        </table></td>
+                      </tr>
+                      <tr>
+                        <td style="text-align: center">** Tarikh Masuk Kuarters hendaklah tidak melebihi 30 hari dari tarikh borang ini</td>
+                      </tr>
+                      <tr>
+                        <td style="text-align: center">&nbsp;</td>
+                      </tr>
+                      <tr>
+                        <td style="text-align: justify">Saya faham bahawa saya bertanggungjawab untuk mengemukakan Sijil Akuan Masuk</td>
+                      </tr>
+                      <tr>
+                        <td style="text-align: justify">Rumah dalam masa 3 hari bekerja dari Tarikh Masuk Kuarters yang dinyatakan di atas.</td>
+                      </tr>
+                      <tr>
+                      <td style="text-align: justify">Kegagalan saya untuk mengemukakan Sijil Akuan Masuk Rumah mengikut tempoh yang</td>
+                      </tr>
+                      <tr>
+                        <td style="text-align: justify">ditetapkan boleh menyebabkan kelulusan/tawaran menduduki kuarters terbatal. Saya</td>
+                      </tr>
+                      <tr>
+                        <td style="text-align: justify">bertanggungjawab untuk memastikan bayaran sewa kuarters atau penolakan gaji bagi</td>
+                      </tr>
+                      <tr>
+                        <td style="text-align: justify">pembayaran menduduki kuarters bermula dari tarikh Sijil Akuan Masuk Rumah yang akan</td>
+                      </tr>
+                      <tr>
+                        <td style="text-align: justify">saya kemukakan.</td>
+                      </tr>
+                      </tbody>
+                  </table></td>
+                </tr>
+              </tbody>
+            </table></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td>Pengesahan Pemohon :</td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tbody>
+                <tr>
+                  <td width="32%" rowspan="4" valign="top" style="tdborber">Tandatangan :</td>
+                  <td width="68%">&nbsp;</td>
+				
+                </tr>
+                <tr>
+                  <td>Nama : _______________________________________</td>
+                </tr>
+                <tr>
+                  <td>&nbsp;</td>
+                </tr>
+                <tr>
+                  <td>Tarikh : _______________________________________</td>
+                </tr>
+              </tbody>
+            </table></td>
+          </tr>
+        </tbody>
+      </table></td>
+    </tr>
+  </tbody>
+</table>
+	
+</body>
+</html>
