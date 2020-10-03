cbe2eb02017f4d198eaee3bd50ee4690ca14bce5
mohdkairulnizam <64393569+mohdkairulnizam@users.noreply.github.com>
2020-10-03 14:27:11 +0800

    surat aku janji

---
+++ resources/views/surat-akujanji.blade.php
@@ -0,0 +1,222 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<link rel="stylesheet" href="pdf.css">
+</head>
+
+<body>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tbody>
+    <tr>
+      <td height="37" align="right">BRG/KRTRS – AKU JANJI MASUK KUARTERS</td>
+    </tr>
+    <tr>
+      <td height="333" class="tdborder"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+        <tbody>
+          <tr>
+            <td style="text-align: center">&nbsp;</td>
+          </tr>
+          <tr>
+            <td style="text-align: center"><u><strong>SURAT AKU JANJI PENGHUNI KUARTERS</strong></u></td>
+          </tr>
+          <tr>
+            <td style="text-align: center"><u><strong>JABATAN TENAGA MANUSIA</strong></u></td>
+          </tr>
+          <tr>
+            <td align="justify">&nbsp;</td>
+          </tr>
+          <tr>
+            <td align="justify">&nbsp;</td>
+          </tr>
+          </tbody>
+        <tbody>
+          <tr>
+            <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tbody>
+                <tr>
+                  <td>&nbsp;</td>
+                  <td align="justify">Saya______________________________________ No. Kad Pengenalan _________________________ menghuni kuarters di unit bernombor_______________ di (ILJTM) _______________________________ dengan sesungguhnya berjanji bahawa saya akan mematuhi Perintah Am Bab E berkaitan Kuarters Kerajaan dan semua peraturan-peraturan lain berkaitan Kuarters Kerajaan yang berkuatkuasa dari semasa ke semasa samada yang dikeluarkan oleh agensi di peringkat pusat mahupun yang dikeluarkan oleh JTM atau pihak ILJTM secara dalaman. Adalah menjadi tanggungjawab saya untuk memastikan saya sentiasa prihatin dan faham semua peraturan yang dikeluarkan dan dikuatkuasakan dari semasa ke semasa di sepanjang saya menduduki kuarters. Secara umumnya, saya berjanji untuk: </td>
+                  <td>&nbsp;</td>
+                </tr>
+                <tr>
+                  <td>&nbsp;</td>
+                  <td align="justify">&nbsp;</td>
+                  <td>&nbsp;</td>
+                </tr>
+                <tr>
+                  <td>&nbsp;</td>
+                  <td align="justify"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tbody>
+                      <tr>
+                        <td width="17%" align="center" valign="top">i)</td>
+                        <td width="83%" align="justify">Memastikan segala bayaran berkaitan pendudukan kuarters dijelaskan mengikut kadar yang sepatutnya mengikut tempoh yang ditetapkan;</td>
+                        </tr>
+                      </tbody>
+                    </table></td>
+                  <td>&nbsp;</td>
+                </tr>
+                <tr>
+                  <td>&nbsp;</td>
+                  <td align="justify"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tbody>
+                      <tr>
+                        <td width="17%" align="center" valign="top">ii)</td>
+                        <td width="83%" align="justify">Memahami bahawa Kerajaan berhak membuat potongan gaji untuk bayaran yang perlu dijelaskan di sepanjang pendudukan kuarters dan Kerajaan berhak pada bila-bila masa membuat tuntutan bayaran sekiranya bayaran yang dijelaskan melalui potongan gaji kurang dari kadar yang sepatutnya;</td>
+                        </tr>
+                      </tbody>
+                    </table></td>
+                  <td>&nbsp;</td>
+                </tr>
+                <tr>
+                  <td>&nbsp;</td>
+                  <td align="justify"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tbody>
+                      <tr>
+                        <td width="17%" align="center" valign="top">iii)</td>
+                        <td width="83%" align="justify">Memastikan diri saya sentiasa layak menduduki kuarters mengikut peraturan semasa yang berkuatkuasa dan bersedia dikenakan bayaran tambahan oleh Kerajaan sekiranya saya masih menduduki kuarters setelah hilang kelayakan menduduki kuarters;</td>
+                        </tr>
+                      </tbody>
+                    </table></td>
+                  <td>&nbsp;</td>
+                </tr>
+                <tr>
+                  <td>&nbsp;</td>
+                  <td align="justify"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tbody>
+                      <tr>
+                        <td width="17%" align="center" valign="top">iv)</td>
+                        <td width="83%" align="justify">Menjaga semua aset Kerajaan samada Aset Alih atau Aset Tak Alih di dalam kuarters yang saya duduki termasuk kawasan sekitar. Saya bertanggungjawab untuk melaporkan sebarang kerosakan atau kehilangan dan menanggung kerosakan atau kehilangan aset Kerajaan yang disebabkan oleh kecuaian diri saya atau keluarga; dan</td>
+                        </tr>
+                      </tbody>
+                    </table></td>
+                  <td>&nbsp;</td>
+                </tr>
+                <tr>
+                  <td>&nbsp;</td>
+                  <td align="justify"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tbody>
+                      <tr>
+                        <td width="17%" align="center" valign="top">v)</td>
+                        <td width="83%" align="justify">Menjaga kebersihan persekitaran dan keharmonian dengan penghuni kuarters yang lain supaya suasana dan persekitaran kuarters aman, selesa dan selamat untuk diduduki.</td>
+                        </tr>
+                      </tbody>
+                    </table></td>
+                  <td>&nbsp;</td>
+                </tr>
+                <tr>
+                  <td>&nbsp;</td>
+                  <td align="justify">&nbsp;</td>
+                  <td>&nbsp;</td>
+                </tr>
+                <tr>
+                  <td>&nbsp;</td>
+                  <td align="justify">Saya sesungguhnya faham bahawa jika saya melanggar Aku Janji ini serta mana-mana peraturan semasa yang berkuatkuasa, saya boleh dikenakan arahan keluar kuarters dan tindakan tatatertib mengikut peraturan semasa yang berkuatkuasa.</td>
+                  <td>&nbsp;</td>
+                </tr>
+                <tr>
+                  <td>&nbsp;</td>
+                  <td align="justify">&nbsp;</td>
+                  <td>&nbsp;</td>
+                </tr>
+                <tr>
+                  <td>&nbsp;</td>
+                  <td align="justify"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tbody>
+                      <tr>
+                        <td align="center">&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td align="center">&nbsp;</td>
+                      </tr>
+                      <tr>
+                        <td align="center">&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td align="center">&nbsp;</td>
+                      </tr>
+                      <tr>
+                        <td align="center">___________________________</td>
+                        <td>&nbsp;</td>
+                        <td align="center">_______________________</td>
+                        </tr>
+                      <tr>
+                        <td align="center">(Tandatangan Pegawai)</td>
+                        <td>&nbsp;</td>
+                        <td align="center">(Jawatan Pegawai)</td>
+                        </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td align="center">Di hadapan :</td>
+                        <td>&nbsp;</td>
+                        </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td align="center">&nbsp;</td>
+                        <td>&nbsp;</td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td align="center">&nbsp;</td>
+                        <td>&nbsp;</td>
+                        </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td align="center">&nbsp;</td>
+                        <td>&nbsp;</td>
+                        </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td align="center" valign="bottom" >_____________________________________</td>
+                        <td>&nbsp;</td>
+                        </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td align="center">(Tandatangan Pegawai Perumahan)</td>
+                        <td>&nbsp;</td>
+                        </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td align="center">&nbsp;</td>
+                        <td>&nbsp;</td>
+                        </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td align="center">&nbsp;</td>
+                        <td>&nbsp;</td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td align="center">&nbsp;</td>
+                        <td>&nbsp;</td>
+                        </tr>
+                      <tr>
+                        <td align="center">______________________</td>
+                        <td colspan="2" align="center">__________________________________________________</td>
+                        </tr>
+                      <tr>
+                        <td align="center">(Tarikh)</td>
+                        <td colspan="2" align="center">(Cop Pegawai Perumahan &amp; Cop Rasmi Jabatan</td>
+                        </tr>
+                      </tbody>
+                    </table></td>
+                  <td>&nbsp;</td>
+                </tr>
+                <tr>
+                  <td width="3%">&nbsp;</td>
+                  <td width="86%" align="justify"><blockquote>
+                    <blockquote>&nbsp;</blockquote>
+                  </blockquote></td>
+                  <td width="3%">&nbsp;</td>
+                </tr>
+              </tbody>
+            </table></td>
+          </tr>
+        </tbody>
+        <tbody>
+        </tbody>
+      </table></td>
+    </tr>
+  </tbody>
+</table>
+</body>
+</html>
