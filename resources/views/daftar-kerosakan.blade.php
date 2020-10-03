220da43ccd53b5c26016dbc424d6029e90925c3c
mohdkairulnizam <64393569+mohdkairulnizam@users.noreply.github.com>
2020-10-03 15:25:27 +0800

    daftar kerosakan

---
+++ resources/views/daftar-kerosakan.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<link rel="stylesheet" href="pdf.css">
+</head>
+
+<body>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tbody>
+    <tr>
+      <td height="37" colspan="13" align="right">BRG/KRTRS – DAFTAR KEROSAKAN &amp; PENYELENGGARAAN</td>
+    </tr>
+    <tr>
+      <td rowspan="3" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>BIL.</strong></td>
+      <td height="37" colspan="2" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>JENIS</strong></td>
+      <td colspan="2" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>MAKLUMAT ADUAN SEKIRANYA PENYELENGGARAAN PEMBAIKAN</strong></td>
+      <td rowspan="3" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>NO. UNIT  KUARTERS  YANG  TERLIBAT</strong></td>
+      <td colspan="2" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>KATEGORI</strong></td>
+      <td rowspan="3" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>KETERANGAN  KEROSAKAN /  PEMBAIKAN  ** Tambah  lampiran  sekiranya  ruang tidak  mencukupi </strong></td>
+      <td colspan="3" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>TINDAKAN JABATAN</strong></td>
+      <td rowspan="3" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>CATATAN</strong></td>
+    </tr>
+    <tr>
+      <td rowspan="2" align="center" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>PENCEGAHAN</strong></td>
+      <td rowspan="2" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>PEMBAIKAN</strong></td>
+      <td rowspan="2" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>TARIKH  ADUAN </strong></td>
+      <td rowspan="2" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>NAMA  PENGADU</strong></td>
+      <td rowspan="2" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>ASET  ALIH</strong></td>
+      <td rowspan="2" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>ASET  TAK  ALIH</strong></td>
+      <td height="37" colspan="2" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>NILAI PENYELENGGARAAN</strong></td>
+      <td rowspan="2" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>TARIKH SELESAI</strong></td>
+    </tr>
+    <tr>
+      <td align="right" class="tdborder" bgcolor="#828282"><strong>ANGGARAN NILAI (RM)</strong></td>
+      <td align="right" class="tdborder" bgcolor="#828282"><strong>NILAI SEBENAR (RM)</strong></td>
+    </tr>
+  </tbody>
+</table>
+</body>
+</html>
