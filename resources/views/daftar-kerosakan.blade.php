8ea4a49ed7f1f6063f06084f3d839edc65189759
mohdkairulnizam <64393569+mohdkairulnizam@users.noreply.github.com>
2020-10-03 17:23:20 +0800

    daftar kerosakan

--- resources/views/daftar-kerosakan.blade.php
+++ resources/views/daftar-kerosakan.blade.php
@@ -2,41 +2,68 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Untitled Document</title>
+<title>daftar-kerosakan</title>
 <link rel="stylesheet" href="pdf.css">
 </head>
 
-<body>
-<table width="100%" border="0" cellspacing="0" cellpadding="0">
+<body><table width="100%" border="0" cellspacing="0" cellpadding="0" class="tdborder" align="center">
   <tbody>
     <tr>
-      <td height="37" colspan="13" align="right">BRG/KRTRS – DAFTAR KEROSAKAN &amp; PENYELENGGARAAN</td>
+      <th rowspan="3" scope="col" bgcolor="#ADADAD">BIL.</th>
+      <th colspan="2" scope="col" bgcolor="#ADADAD">JENIS</th>
+      <th colspan="2" scope="col" bgcolor="#ADADAD">MAKLUMAT ADUAN SEKIRANYA PENYELENGGARAAN PEMBAIKAN</th>
+      <th rowspan="3" scope="col" bgcolor="#ADADAD">NO. UNIT KUARTERS YANG TERLIABAT</th>
+      <th colspan="2" scope="col" bgcolor="#ADADAD">KATEGORI</th>
+      <th rowspan="3" scope="col" bgcolor="#ADADAD" >KETERANGAN KEROSAKAN / PEMBAIKAN ** Tambah lampiran sekiranya ruang tidak mencukupi</th>
+      <th colspan="3" scope="col" bgcolor="#ADADAD">TINDAKAN JABATAN</th>
+      <th rowspan="3" scope="col" bgcolor="#ADADAD">CATATAN</th>
     </tr>
     <tr>
-      <td rowspan="3" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>BIL.</strong></td>
-      <td height="37" colspan="2" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>JENIS</strong></td>
-      <td colspan="2" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>MAKLUMAT ADUAN SEKIRANYA PENYELENGGARAAN PEMBAIKAN</strong></td>
-      <td rowspan="3" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>NO. UNIT  KUARTERS  YANG  TERLIBAT</strong></td>
-      <td colspan="2" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>KATEGORI</strong></td>
-      <td rowspan="3" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>KETERANGAN  KEROSAKAN /  PEMBAIKAN  ** Tambah  lampiran  sekiranya  ruang tidak  mencukupi </strong></td>
-      <td colspan="3" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>TINDAKAN JABATAN</strong></td>
-      <td rowspan="3" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>CATATAN</strong></td>
+      <td rowspan="2" bgcolor="#ADADAD"><strong>PENCEGAHAN</strong></td>
+      <td rowspan="2" bgcolor="#ADADAD"><strong>PEMBAIKAN</strong></td>
+      <td rowspan="2" bgcolor="#ADADAD"><strong>TARIKH ADUAN</strong></td>
+      <td rowspan="2" bgcolor="#ADADAD"><strong>NAMA PENGADU</strong></td>
+      <td rowspan="2" bgcolor="#ADADAD"><strong>ASET ALIH</strong></td>
+      <td rowspan="2" bgcolor="#ADADAD"><strong>ASET TAK ALIH</strong></td>
+      <td colspan="2" bgcolor="#ADADAD"><strong>NILAI PENYELENGGARAAN</strong></td>
+      <td rowspan="2" bgcolor="#ADADAD"><strong>TARIKH SELESAI</strong></td>
     </tr>
     <tr>
-      <td rowspan="2" align="center" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>PENCEGAHAN</strong></td>
-      <td rowspan="2" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>PEMBAIKAN</strong></td>
-      <td rowspan="2" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>TARIKH  ADUAN </strong></td>
-      <td rowspan="2" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>NAMA  PENGADU</strong></td>
-      <td rowspan="2" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>ASET  ALIH</strong></td>
-      <td rowspan="2" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>ASET  TAK  ALIH</strong></td>
-      <td height="37" colspan="2" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>NILAI PENYELENGGARAAN</strong></td>
-      <td rowspan="2" align="right" class="tdborder" style="text-align: center" bgcolor="#828282"><strong>TARIKH SELESAI</strong></td>
+      <td height="137" bgcolor="#ADADAD"><strong>ANGGARAN</strong></td>
+      <td bgcolor="#ADADAD"><strong>NILAI SEBENAR</strong></td>
     </tr>
     <tr>
-      <td align="right" class="tdborder" bgcolor="#828282"><strong>ANGGARAN NILAI (RM)</strong></td>
-      <td align="right" class="tdborder" bgcolor="#828282"><strong>NILAI SEBENAR (RM)</strong></td>
+      <th scope="row">&nbsp;</th>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <th scope="row">&nbsp;</th>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
     </tr>
   </tbody>
 </table>
+
 </body>
 </html>
