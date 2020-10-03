3eabde17b1e1840305cb0b293876368018ca4f4e
mohdkairulnizam <64393569+mohdkairulnizam@users.noreply.github.com>
2020-10-03 22:35:54 +0800

    daftar-kerosakan

--- resources/views/daftar-kerosakan.blade.php
+++ resources/views/daftar-kerosakan.blade.php
@@ -15,28 +15,28 @@
       <td height="37" colspan="13" scope="col" align="center"><strong>DAFTAR KEROSAKAN DAN PENYELENGGARAAN KUARTERS</strong></td>
     </tr>
     <tr>
-      <th rowspan="3" scope="col" bgcolor="#ADADAD" class="tdborder">BIL.</th>
-      <th colspan="2" scope="col" bgcolor="#ADADAD" class="tdborder">JENIS</th>
-      <th colspan="2" scope="col" bgcolor="#ADADAD" class="tdborder">MAKLUMAT ADUAN SEKIRANYA PENYELENGGARAAN PEMBAIKAN</th>
-      <th rowspan="3" scope="col" bgcolor="#ADADAD" class="tdborder">NO. UNIT KUARTERS YANG TERLIABAT</th>
-      <th colspan="2" scope="col" bgcolor="#ADADAD" class="tdborder">KATEGORI</th>
-      <th rowspan="3" scope="col" bgcolor="#ADADAD" class="tdborder">KETERANGAN KEROSAKAN / PEMBAIKAN ** Tambah lampiran sekiranya ruang tidak mencukupi</th>
-      <th colspan="3" scope="col" bgcolor="#ADADAD" class="tdborder">TINDAKAN JABATAN</th>
-      <th rowspan="3" scope="col" bgcolor="#ADADAD" class="tdborder">CATATAN</th>
+      <th rowspan="3" scope="col" bgcolor="#ADADAD" class="tdborder" align="center">BIL.</th>
+      <th height="157" colspan="2" bgcolor="#ADADAD" class="tdborder" scope="col" align="center">JENIS</th>
+      <th colspan="2" scope="col" bgcolor="#ADADAD" class="tdborder" align="center">MAKLUMAT ADUAN SEKIRANYA PENYELENGGARAAN PEMBAIKAN</th>
+      <th rowspan="3" scope="col" bgcolor="#ADADAD" class="tdborder" align="center">NO. UNIT KUARTERS YANG TERLIABAT</th>
+      <th colspan="2" scope="col" bgcolor="#ADADAD" class="tdborder" align="center">KATEGORI</th>
+      <th rowspan="3" scope="col" bgcolor="#ADADAD" class="tdborder" align="center">KETERANGAN KEROSAKAN / PEMBAIKAN ** Tambah lampiran sekiranya ruang tidak mencukupi</th>
+      <th colspan="3" scope="col" bgcolor="#ADADAD" class="tdborder" align="center">TINDAKAN JABATAN</th>
+      <th rowspan="3" scope="col" bgcolor="#ADADAD" class="tdborder" align="center">CATATAN</th>
     </tr>
     <tr>
-      <th rowspan="2" bgcolor="#ADADAD" class="tdborder"><strong>PENCEGAHAN</strong></th>
+      <th rowspan="2" bgcolor="#ADADAD" class="tdborder" align="center"><strong>PENCEGAHAN</strong></th>
       <td rowspan="2" bgcolor="#ADADAD" class="tdborder" ><strong>PEMBAIKAN</strong></td>
-      <td rowspan="2" bgcolor="#ADADAD" class="tdborder"><strong>TARIKH ADUAN</strong></td>
-      <td rowspan="2" bgcolor="#ADADAD" class="tdborder"><strong>NAMA PENGADU</strong></td>
-      <td rowspan="2" bgcolor="#ADADAD" class="tdborder"><strong>ASET ALIH</strong></td>
-      <td rowspan="2" bgcolor="#ADADAD" class="tdborder"><strong>ASET TAK ALIH</strong></td>
-      <td colspan="2" bgcolor="#ADADAD" class="tdborder"><strong>NILAI PENYELENGGARAAN</strong></td>
-      <td rowspan="2" bgcolor="#ADADAD" class="tdborder"><strong>TARIKH SELESAI</strong></td>
+      <td rowspan="2" bgcolor="#ADADAD" class="tdborder" align="center"><strong>TARIKH ADUAN</strong></td>
+      <td rowspan="2" bgcolor="#ADADAD" class="tdborder" align="center"><strong>NAMA PENGADU</strong></td>
+      <td rowspan="2" bgcolor="#ADADAD" class="tdborder" align="center"><strong>ASET ALIH</strong></td>
+      <td rowspan="2" bgcolor="#ADADAD" class="tdborder" align="center"><strong>ASET TAK ALIH</strong></td>
+      <td colspan="2" bgcolor="#ADADAD" class="tdborder" align="center"><strong>NILAI PENYELENGGARAAN</strong></td>
+      <td rowspan="2" bgcolor="#ADADAD" class="tdborder" align="center"><strong>TARIKH SELESAI</strong></td>
     </tr>
     <tr>
-      <td height="174" bgcolor="#ADADAD" class="tdborder"><strong>ANGGARAN</strong></td>
-      <td bgcolor="#ADADAD" class="tdborder"><strong>NILAI SEBENAR</strong></td>
+      <td height="174" bgcolor="#ADADAD" class="tdborder" align="center"><strong>ANGGARAN</strong></td>
+      <td bgcolor="#ADADAD" class="tdborder" align="center"><strong>NILAI SEBENAR</strong></td>
     </tr>
     <tr>
       <td height="45" class="tdborder">    
