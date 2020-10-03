5afd1050b016ac8f6b30cf80c2b39629a4f11e41
mohdkairulnizam <64393569+mohdkairulnizam@users.noreply.github.com>
2020-10-04 01:12:37 +0800

    borang rayuan

--- resources/views/borang-rayuan.blade.php
+++ resources/views/borang-rayuan.blade.php
@@ -22,7 +22,7 @@
             <td class="bg tdborder"><table width="100%" border="0" cellspacing="0" cellpadding="5">
               <tbody>
                 <tr>
-                  <td class="bg" style="text-align: center">BORANG RAYUAN MENGENAI PENDUDUKAN KUARTERS</td>
+                  <td class="bg" style="text-align: center"><strong>BORANG RAYUAN MENGENAI PENDUDUKAN KUARTERS</strong></td>
                 </tr>
               </tbody>
             </table></td>
@@ -31,7 +31,7 @@
             <td>&nbsp;</td>
           </tr>
           <tr>
-            <td>NAMA PREMIS : __________________________________________________________________________</td>
+            <td><strong>NAMA PREMIS : </strong>__________________________________________________________________________</td>
           </tr>
           <tr>
             <td>&nbsp;</td>
@@ -40,17 +40,15 @@
             <td><table width="70%" border="0" align="center" cellpadding="5" cellspacing="0">
               <tbody>
                 <tr>
-                  <td class="bg" style="text-align: right"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                  <td class="bg border ; tdborder" style="text-align: right"><table width="100%" border="0" cellspacing="0" cellpadding="0">
                     <tbody>
                       <tr>
-                        <td width="61%">NO. UNIT/RUMAH KUARTERS </td>
-                        <td width="2%" rowspan="2" class="">&nbsp;</td>
-                        <td width="37%" rowspan="2" class="tdborder" style="background: white">&nbsp;</td>
+                        <td width="47%"><p><strong>NO. UNIT/RUMAH KUARTERS </strong></p>
+                          <p><strong>YANG SEDANG DIDUDUKI </strong></p></td>
+                        <td width="4%" class="">&nbsp;</td>
+                        <td width="49%" class="tdborder" style="background: white">&nbsp;</td>
                       </tr>
-                      <tr>
-                        <td>YANG SEDANG DIDUDUKI </td>
-                        </tr>
-                    </tbody>
+                      </tbody>
                   </table></td>
                 </tr>
               </tbody>
@@ -63,45 +61,54 @@
             <td><table width="100%" border="0" cellspacing="0" cellpadding="5">
               <tbody>
                 <tr>
-                  <td class="bg tdborder">(A) MAKLUMAT PEMOHON</td>
+                  <td class="bg tdborder"><strong>(A) MAKLUMAT PEMOHON</strong></td>
                 </tr>
                 <tr>
                   <td class="tdborder"><table width="100%" border="0" cellspacing="0" cellpadding="5">
                     <tbody>
                       <tr>
-                        <td colspan="3">&nbsp;</td>
-                      </tr>
-                      <tr>
                         <td colspan="3"><table width="100%" border="0" cellspacing="0" cellpadding="0">
                           <tbody>
                             <tr>
-                              <td>1. Nama</td>
-                            </tr>
-                            <tr>
-                              <td class="tdborder">&nbsp;</td>
-                            </tr>
-                          </tbody>
-                        </table></td>
-                      </tr>
-                      <tr>
-                        <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
-                          <tbody>
-                            <tr>
-                              <td>2. No Kad Pengenalan</td>
+                              <td><p>&nbsp;</p>
+                                <p>1. Nama</p></td>
                               </tr>
                             <tr>
-                              <td class="tdborder">&nbsp;</td>
+                              <td height="30" class="tdborder"><p>&nbsp;</p>
+                                <p>&nbsp;</p></td>
                               </tr>
                             </tbody>
                           </table></td>
-                        <td>&nbsp;</td>
-                        <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                      </tr>
+                      <tr>
+                        <td width="47%"><table width="100%" border="0" cellspacing="0" cellpadding="0">
                           <tbody>
                             <tr>
-                              <td>3. No Telefon Bimbit</td>
+                              <td><p>&nbsp;</p>
+                                <p>2. No Kad Pengenalan</p></td>
                               </tr>
                             <tr>
-                              <td class="tdborder">&nbsp;</td>
+                              <td width="93%" height="30"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                <tbody>
+                                  <tr>
+                                    <td class="tdborder"><p>&nbsp;</p>
+                                      <p>&nbsp;</p></td>
+                                  </tr>
+                                </tbody>
+                              </table></td>
+                              </tr>
+                            </tbody>
+                          </table></td>
+                        <td width="4%">&nbsp;</td>
+                        <td width="49%"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                          <tbody>
+                            <tr>
+                              <td><p>&nbsp;</p>
+                                <p>3. No Telefon Bimbit</p></td>
+                              </tr>
+                            <tr>
+                              <td height="30" class="tdborder"><p>&nbsp;</p>
+                                <p>&nbsp;</p></td>
                               </tr>
                             </tbody>
                           </table></td>
@@ -113,19 +120,19 @@
                               <td colspan="2">4. Status Pemohon</td>
                               </tr>
                             <tr>
-                              <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                              <td width="34%"><table width="100%" border="0" cellspacing="0" cellpadding="0">
                                 <tbody>
                                   <tr>
-                                    <td class="checkbox">&nbsp;</td>
+                                    <td class="checkbox" height="25">&nbsp;</td>
                                     <td>&nbsp;&nbsp;Penama Kuarters</td>
                                     </tr>
                                   </tbody>
                                 </table></td>
-                              <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                              <td width="66%"><table width="100%" border="0" cellspacing="0" cellpadding="0">
                                 <tbody>
                                   <tr>
-                                    <td class="checkbox">&nbsp;</td>
-                                    <td>&nbsp;&nbsp;Penama Kuarters</td>
+                                    <td class="checkbox" height="25">&nbsp;</td>
+                                    <td>&nbsp;&nbsp;Bukan Penama Kuarters (Hubungan: _________________)</td>
                                     </tr>
                                   </tbody>
                                 </table></td>
@@ -137,9 +144,232 @@
                   </table></td>
                 </tr>
                 <tr>
-                  <td class="bg tdborder">(B) JENIS RAYUAN</td>
+                  <td  align="left" class="bg border"><strong>(B) JENIS RAYUAN</strong></td>
                 </tr>
-              </tbody>
+                <tr>
+                  <td valign="top" class="tdborder"><p>&nbsp;</p>
+                    <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tbody>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                          <tbody>
+                            <tr>
+                              <td width="9%"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                <tbody>
+                                  <tr>
+                                    <td class="tdborder" valign="top" height="30">&nbsp;</td>
+                                  </tr>
+                                </tbody>
+                              </table></td>
+                              <td width="3%">&nbsp;</td>
+                              <td width="88%"><p>Menduduki Rumah Khas melebihi tempoh kelayakan (Max: 30 hari)<br>
+                                </p>
+                                <p><em style="font-size: 12px">** Rujukan: para 2.5.8, Garis Panduan Pengurusan Kuarters ILJTM</em></p></td>
+                            </tr>
+                          </tbody>
+                        </table></td>
+                        <td>&nbsp;</td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                          <tbody>
+                            <tr>
+                              <td width="9%"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                <tbody>
+                                  <tr>
+                                    <td class="tdborder" valign="top" height="30">&nbsp;</td>
+                                  </tr>
+                                </tbody>
+                              </table></td>
+                              <td width="3%">&nbsp;</td>
+                              <td width="88%"><p>Menduduki Rumah Khas melebihi tempoh kelayakan (Max: 30 hari)<br>
+                                </p>
+                                <p><em style="font-size: 12px">** Rujukan: para 2.5.8, Garis Panduan Pengurusan Kuarters ILJTM</em></p></td>
+                            </tr>
+                          </tbody>
+                        </table></td>
+                        <td>&nbsp;</td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                          <tbody>
+                            <tr>
+                              <td width="7%" valign="top"><table width="100%" height="85" border="1" cellpadding="0" cellspacing="0">
+                                <tbody>
+                                  <tr>
+                                    <td valign="top" height="30">&nbsp;</td>
+                                  </tr>
+                                </tbody>
+                              </table></td>
+                              <td width="3%">&nbsp;</td>
+                              <td width="90%"><p>Menduduki Rumah Biasa selepas menerima kunci rumah sendiri dalam lingkungan 25km </p>
+                                <p>untuk menguruskan pindahan (Max: 45hari)<br>
+                                  </p>
+                                <p><em style="font-size: 12px">** Rujukan: para 2.6.10, Garis Panduan Pengurusan Kuarters ILJTM</em></p></td>
+                            </tr>
+                          </tbody>
+                        </table></td>
+                        <td>&nbsp;</td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                          <tbody>
+                            <tr>
+                              <td width="9%"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                <tbody>
+                                  <tr>
+                                    <td class="tdborder" valign="top" height="30">&nbsp;</td>
+                                  </tr>
+                                </tbody>
+                              </table></td>
+                              <td width="3%">&nbsp;</td>
+                              <td width="88%"><p>Menduduki Rumah Biasa sepanjang cuti tanpa/separuh gaji dalam negara<br>
+                                </p>
+                                <p><em style="font-size: 12px">** Rujukan: para 2.6.9, Garis Panduan Pengurusan Kuarters ILJTM</em></p></td>
+                            </tr>
+                          </tbody>
+                        </table></td>
+                        <td>&nbsp;</td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                          <tbody>
+                            <tr>
+                              <td width="10%" align="center"><table width="44%" border="0" cellspacing="0" cellpadding="0">
+                                <tbody>
+                                  <tr>
+                                    <td class="tdborder" valign="top" height="30">&nbsp;</td>
+                                  </tr>
+                                </tbody>
+                              </table></td>
+                              <td width="3%">&nbsp;</td>
+                              <td width="87%"><p>Menduduki Rumah Biasa walaupun mempunyai rumah sendiri dalam lingkungan 25km </p>
+                                <p>(bayaran dua kali ganda)</p>
+                                <p><em style="font-size: 12px">** Rujukan: para 2.6.10, Garis Panduan Pengurusan Kuarters ILJTM</em></p></td>
+                            </tr>
+                          </tbody>
+                        </table></td>
+                        <td>&nbsp;</td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                          <tbody>
+                            <tr>
+                              <td width="11%"><table width="85%" border="0" cellspacing="0" cellpadding="0">
+                                <tbody>
+                                  <tr>
+                                    <td class="tdborder" valign="top" height="30">&nbsp;</td>
+                                  </tr>
+                                </tbody>
+                              </table></td>
+                              <td width="3%">&nbsp;</td>
+                              <td width="86%"><p>Tukar unit kuarters yang diduduki</p>
+                                <p><em style="font-size: 12px">** Rujukan: para 3.1.5, Garis Panduan Pengurusan Kuarters ILJTM</em></p></td>
+                            </tr>
+                          </tbody>
+                        </table></td>
+                        <td>&nbsp;</td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                    <p>&nbsp;</p>
+                    <p>&nbsp;</p></td>
+                </tr>
+                <tr>
+                  <td class="tdborder">&nbsp;</td>
+                </tr>
+                <tr>
+                  <td class="tdborder"><p>&nbsp;</p></td>
+                </tr>
+                <tr>
+                  <td class="bg tdborder" ><strong>(C) PERAKUAN PEMOHON</strong></td>
+                </tr>
+                <tr>
+                  <td class="tdborder" align="justify"><p>&nbsp;</p>
+                    <p>Saya mengesahkan bahawa bersama-sama borang ini, saya telah melampirkan Surat Rayuan yang menerangkan justifikasi permohonan rayuan ini berserta dengan lampiran dokumen berkaitan seperti yang diperlukan oleh pihak institut. Saya turut mengesahkan bahawa semua maklumat yang dibekalkan bersama permohonan ini adalah maklumat yang tepat dan benar. Saya faham bahawa kelulusan permohonan adalah tertakluk kepada pertimbangan pihak pengurusan institut sepenuhnya.</p>
+                    <p>&nbsp;</p>
+                    <table width="100%" border="0" cellspacing="0" cellpadding="4">
+                      <tbody>
+                        <tr>
+                          <td width="40%" rowspan="3" valign="top" class="tdborder" ><em style="font-size: 12px">Tandatangan</em> :</td>
+                          <td width="70%">&nbsp;&nbsp;Nama : _______________________________________________</td>
+                        </tr>
+                        <tr>
+                          <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                          <td>&nbsp;&nbsp;Tarikh : ______________________________________________</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <p>&nbsp;</p></td>
+                </tr>
+                <tr>
+                  <td class="bg border; tdborder "><strong><em>UNTUK KEGUNAAN PEJABAT</em></strong></td>
+                </tr>
+                <tr>
+                  <td height="309" class="tdborder"><table width="100%" border="0" cellspacing="0" cellpadding="0" align="center">
+                    <tbody>
+                      <tr>
+                        <td width="40%" style="text-align: center"><p>&nbsp;</p>
+                          <p>Tarikh Permohonan Diterima:</p></td>
+                        <td width="3%" rowspan="4" valign="top">&nbsp;</td>
+                        
+                        <td width="57%" rowspan="4" valign="top" class="tdborder"><em style="font-size: 12px"><strong>Tandatangan &amp; Cop Penyelia Kuarters:</strong></em></td>
+                        </tr>
+                      <tr>
+                        <td class="tdborder"><p>&nbsp;</p>
+                          <p>&nbsp;</p></td>
+                        </tr>
+                      <tr>
+                        <td style="text-align: center"><p>&nbsp;</p>
+                          <p>No. Rujukan Diberikan:</p></td>
+                        </tr>
+                      <tr>
+                        <td class="tdborder"><p>&nbsp;</p>
+                          <p>&nbsp;</p></td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <p>&nbsp;</p></td>
+                </tr>
+                </tbody>
             </table></td>
           </tr>
         </tbody>
