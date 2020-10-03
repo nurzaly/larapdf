6c1948a7a18590cfeed10d5e0c7a8f846c51f164
mohdkairulnizam <64393569+mohdkairulnizam@users.noreply.github.com>
2020-10-03 13:50:01 +0800

    borang aset semasa

---
+++ resources/views/borang-asetsemasaB2.blade.php
@@ -0,0 +1,186 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Borang Pemohonan</title>
+<link rel="stylesheet" href="pdf.css">
+</head>
+
+<body>
+<table width="100%" border="0"  align="center" cellpadding="0" cellspacing="0">
+  <tbody>
+    <tr>
+      <td><table width="100%" border="0" align="center"  cellpadding="0" cellspacing="0">
+        <tbody>
+          <tr>
+            <td style="text-align: right">BRG/KRTRS - KEADAAN ASET SEMASA</td>
+          </tr>
+          <tr>
+            <td style="text-align: left"><u><strong>BORANG KEADAAN ASET SEMASA</strong></u></td>
+          </tr>
+          <tr>
+            <td style="text-align: left;">&nbsp;</td>
+          </tr>
+          <tr>
+            <td style="text-align: left;"><p><strong>(Bahagian 2: Aset Tak Alih termasuk kelengkapan sivil, struktur dll)</strong></p>
+              <p>&nbsp;</p></td>
+          </tr>
+          <tr>
+            <td style="text-align: center"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tbody>
+                <tr>
+                  <td width="8%" height="50" align="center" class="bg border" style="border: 1px solid black"><strong>Bil.</strong></td>
+                  <td width="42%" align="center" class="bg border" style="border: 1px solid black"><strong>Keterangan Aset / Kelengkapan Sivil / Maklumat Struktur</strong></td>
+                  <td width="50%" align="center" class="bg border" style="border: 1px solid black"><strong>Catatan Keadaan Semasa / Kerosakan Aset</strong></td>
+                  </tr>
+                <tr>
+                  <td align="center" style="border: 1px solid black"  height="50
+																			  ">&nbsp;</td>
+                  <td align="center" style="border: 1px solid black" >&nbsp;</td>
+                  <td align="center" style="border: 1px solid black" >&nbsp;</td>
+                </tr>
+                <tr>
+                  <td align="center" style="border: 1px solid black"  height="45">&nbsp;</td>
+                  <td align="center" style="border: 1px solid black" >&nbsp;</td>
+                  <td align="center" style="border: 1px solid black" >&nbsp;</td>
+                </tr>
+                <tr>
+                  <td align="center" style="border: 1px solid black"  height="45">&nbsp;</td>
+                  <td align="center" style="border: 1px solid black" >&nbsp;</td>
+                  <td align="center" style="border: 1px solid black" >&nbsp;</td>
+                </tr>
+                <tr>
+                  <td align="center" style="border: 1px solid black"  height="45">&nbsp;</td>
+                  <td align="center" style="border: 1px solid black" >&nbsp;</td>
+                  <td align="center" style="border: 1px solid black" >&nbsp;</td>
+                </tr>
+                <tr>
+                  <td align="center" style="border: 1px solid black"  height="45">&nbsp;</td>
+                  <td align="center" style="border: 1px solid black" >&nbsp;</td>
+                  <td align="center" style="border: 1px solid black" >&nbsp;</td>
+                </tr>
+                <tr>
+                  <td align="center" style="border: 1px solid black"  height="45">&nbsp;</td>
+                  <td align="center" style="border: 1px solid black" >&nbsp;</td>
+                  <td align="center" style="border: 1px solid black" >&nbsp;</td>
+                </tr>
+                <tr>
+                  <td align="center" style="border: 1px solid black"  height="45">&nbsp;</td>
+                  <td align="center" style="border: 1px solid black" >&nbsp;</td>
+                  <td align="center" style="border: 1px solid black" >&nbsp;</td>
+                </tr>
+                <tr>
+                  <td align="center" style="border: 1px solid black"  height="45">&nbsp;</td>
+                  <td align="center" style="border: 1px solid black" >&nbsp;</td>
+                  <td align="center" style="border: 1px solid black" >&nbsp;</td>
+                </tr>
+                </tbody>
+              </table></td>
+          </tr>
+          <tr>
+            <td style="text-align: left"><em>** Sila tambah sekiranya perlu<br>
+              ** Sila lampirkan gambar sekiranya perlu</em></td>
+          </tr>
+          <tr>
+            <td style="text-align: left">&nbsp;</td>
+          </tr>
+          <tr>
+            <td style="text-align: left" ><u><strong>PENGESAHAN PENGHUNI :</strong></u></td>
+          </tr>
+          <tr>
+     
+          </tr>
+          <tr>
+            <td style="text-align: justify">&nbsp;</td>
+          </tr>
+          <tr>
+            <td style="text-align: justify">Saya mengesahkan bahawa mana-mana aset yang terdapat di dalam unit kuarters saya yang tidak disenaraikan di atas adalah dalam keadaan baik sewaktu saya mula masuk kuarters.</td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td><table width="100%" border="0" cellspacing="0" cellpadding="4">
+              <tbody>
+                <tr>
+                  <td width="35%" rowspan="6" valign="top" class="tdborder" ><em>Tandatangan</em> :</td>
+                  <td width="20%">&nbsp;</td>
+                  <td width="45%">&nbsp;</td>
+                </tr>
+                <tr>
+                  <td>&nbsp;&nbsp;Nama</td>
+                  <td>: _____________________________________</td>
+                </tr>
+                <tr>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                </tr>
+                <tr>
+                  <td>&nbsp;&nbsp;No. Unit Kuarters</td>
+                  <td>: _____________________________________</td>
+                </tr>
+                <tr>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                </tr>
+                <tr>
+                  <td>&nbsp;&nbsp;Tarikh</td>
+                  <td>: ____________________________________</td>
+                </tr>
+              </tbody>
+            </table></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td align="left"><p><u><strong>SEMAKAN PENYELIA KUARTERS ATAU WAKILNYA YANG MENGURUSKAN </strong></u><u><strong>PENYELENGGARAAN:</strong></u></p></td>
+          </tr>
+          <tr>
+            <td align="justify">&nbsp;</td>
+          </tr>
+          <tr>
+            <td align="justify">Saya mengesahkan kerosakan yang disenaraikan oleh penghuni seperti jadual di atas adalah betul pada tarikh yang dinyatakan di bawah.</td>
+          </tr>
+          <tr>
+            <td align="justify">&nbsp;</td>
+          </tr>
+          <tr>
+            <td><table width="100%" border="0" cellspacing="0" cellpadding="4">
+              <tbody>
+                <tr>
+                  <td width="38%" rowspan="6" valign="top" class="tdborder" ><em>Tandatangan</em> :</td>
+                  <td width="20%">&nbsp;</td>
+                  <td width="42%">&nbsp;</td>
+                </tr>
+                <tr>
+                  <td>&nbsp;&nbsp;Nama</td>
+                  <td>: _____________________________________</td>
+                </tr>
+                <tr>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                </tr>
+                <tr>
+                  <td>&nbsp;&nbsp;Jawatan&nbsp;&nbsp;</td>
+                  <td>: _____________________________________</td>
+                </tr>
+                <tr>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                </tr>
+                <tr>
+                  <td>&nbsp;&nbsp;Tarikh</td>
+                  <td>: ____________________________________</td>
+                </tr>
+              </tbody>
+            </table></td>
+          </tr>
+        </tbody>
+      </table></td>
+    </tr>
+  </tbody>
+</table>
+	
+</body>
+</html>
