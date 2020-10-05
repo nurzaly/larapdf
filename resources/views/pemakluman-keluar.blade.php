50b0db2a5332f0cb521b04eda8dd6ec4e633af07
mohdkairulnizam <64393569+mohdkairulnizam@users.noreply.github.com>
2020-10-05 13:19:13 +0800

    borang pemakluman keluar

---
+++ resources/views/pemakluman-keluar.blade.php
@@ -0,0 +1,360 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Borang Pemohonan</title>
+<link rel="stylesheet" href="pdf.css">
+</head>
+
+<body>
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+  <tbody>
+    <tr>
+      <td><table width="100%" border="0"  cellpadding="0" cellspacing="0">
+        <tbody>
+          <tr>
+            <td style="text-align: right; font-size: 12px;">BRG/KRTRS – MOHON KELUAR</td>
+          </tr>
+          <tr>
+            <td style="text-align: center"><table width="100%" border="0" cellspacing="0" cellpadding="5">
+              <tbody>
+                <tr>
+                  <td align="center" class="bg border" style="border: 1px solid black">BORANG PEMAKLUMAN KELUAR KUARTERS</td>
+                </tr>
+              </tbody>
+            </table></td>
+          </tr>
+          <tr>
+            <td style="text-align: left;">&nbsp;</td>
+          </tr>
+          <tr>
+            <td style="text-align: left;">&nbsp;</td>
+          </tr>
+          <tr>
+            <td style="text-align: right"><table width="100%" border="0" cellspacing="0" cellpadding="0" align="left">
+              <tbody>
+                <tr>
+                  <td width="18%" height="30" align="left">Nama Penghuni</td>
+                  <td align="left" width="3%">:</td>
+                  <td><table width="100%" border="0" cellspacing="0" cellpadding="0" align="left" class="tdborder">
+                    <tbody>
+                      <tr>
+                        <td height="25" >&nbsp;</td>
+                      </tr>
+                    </tbody>
+                  </table></td>
+                </tr>
+              </tbody>
+            </table></td>
+          </tr>
+          <tr>
+            <td style="text-align: right">&nbsp;</td>
+          </tr>
+          <tr>
+            <td style="text-align: right"><table width="100%" border="0" cellspacing="0" cellpadding="0" align="left">
+              <tbody>
+                <tr>
+                  <td width="18%" height="30" align="left">No. Unit Kuarters </td>
+                  <td align="left" width="3%">:</td>
+                  <td><table width="100%" border="0" cellspacing="0" cellpadding="0" align="left" class="tdborder">
+                    <tbody>
+                      <tr>
+                        <td height="25" >&nbsp;</td>
+                        </tr>
+                      </tbody>
+                    </table></td>
+                  </tr>
+                </tbody>
+              </table></td>
+          </tr>
+          <tr>
+            <td style="text-align: right"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tbody>
+                <tr>
+                  <td width="58%" height="40">&nbsp;</td>
+                  <td width="42%" align="left">Jenis Kuarters :</td>
+                  </tr>
+                </tbody>
+              </table></td>
+          </tr>
+          <tr>
+            <td style="text-align: left"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tbody>
+                <tr>
+                  <td width="25%" height="30">&nbsp;</td>
+                  <td width="5%" height="30" class="tdborder">&nbsp;</td>
+                  <td align="left">&nbsp;&nbsp;Rumah Biasa :</p>
+                  </blockquote></td>
+                  </tr>
+                </tbody>
+              </table></td>
+          </tr>
+          <tr>
+            <td style="text-align: right" >&nbsp;</td>
+          </tr>
+          <tr>
+            <td style="text-align: right" ><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tbody>
+                <tr>
+                  <td width="25%" height="30">&nbsp;</td>
+                  <td width="5%" height="30" class="tdborder">&nbsp;</td>
+                  <td align="left">&nbsp;&nbsp;Rumah Khas (sila lengkapkan maklumat di bawah)</p>
+                  </blockquote></td>
+                  </tr>
+                </tbody>
+              </table></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td><table width="100%">
+              <tbody>
+                <tr>
+                  <td width="35%" height="30">&nbsp;</td>
+                  <td width="5%">(b)</td>
+                  <td width="20%" >Tugas yang dilantik :</td>
+                  <td width="40%" class="tdborder"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tbody>
+                      <tr>
+                        <td>&nbsp;</td>
+                      </tr>
+                    </tbody>
+                  </table></td>
+                </tr>
+              </tbody>
+            </table></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td><table width="100%">
+              <tbody>
+                <tr>
+                  <td width="35%" height="30">&nbsp;</td>
+                  <td width="5%" >(b)</td>
+                  <td width="20%">Tarikh Akhir lantikan  : </td>
+                  <td width="40%" class="tdborder"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tbody>
+                      <tr>
+                        <td>&nbsp;</td>
+                      </tr>
+                    </tbody>
+                  </table></td>
+                </tr>
+              </tbody>
+            </table></td>
+          </tr>
+          <tr>
+            <td style="text-align: left" >&nbsp;</td>
+          </tr>
+          <tr>
+            <td style="text-align: left" ><table width="93%" border="0" cellspacing="0" cellpadding="0">
+              <tbody>
+                  <tr>
+                    <td width="35%" height="30">&nbsp;</td>
+                    <td width="5%" align="center" >(a)</td>
+                    <td width="20%" align="left">Tarikh Akhir lantikan :</td>
+                    <td width="40%" class="tdborder">&nbsp;</td>
+                  </tr>
+                </tbody>
+            </table></td>
+          </tr>
+          <tr>
+            <td text-decoration="underline">&nbsp;</td>
+          </tr>
+          <tr>
+            <td text-decoration="underline"><table width="93%" border="0" cellspacing="0" cellpadding="0">
+              <tbody>
+                <tr>
+                  <td width="35%" height="30">&nbsp;</td>
+                  <td width="5%" align="center" >(a)</td>
+                  <td width="20%" align="left">No. Rujukan lantikan :</td>
+                  <td width="40%" class="tdborder">&nbsp;</td>
+                </tr>
+              </tbody>
+            </table></td>
+          </tr>
+          <tr>
+            <td text-decoration="underline">&nbsp;</td>
+          </tr>
+          <tr>
+            <td text-decoration="underline"><table width="93%" border="0" cellspacing="0" cellpadding="0">
+              <tbody>
+                <tr>
+                  <td width="35%" height="30">&nbsp;</td>
+                  <td width="5%" align="center" >(a)</td>
+                  <td align="left">Berpindah ke unit kuarters yang lain?</td>
+                  </tr>
+              </tbody>
+            </table></td>
+          </tr>
+          <tr>
+            <td text-decoration="underline">&nbsp;</td>
+          </tr>
+          <tr>
+            <td text-decoration="underline">Saya dengan ini mengesahkan bahawa saya akan keluar kuarters pada;</td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td><table width="68%" border="0" cellspacing="0" cellpadding="0" align="center">
+              <tbody>
+                <tr>
+                  <td width="31%" align="right" height="40"><strong>Tarikh keluar kuarters :</strong></td>
+                  <td width="3%">&nbsp;</td>
+                  <td width="66%" class="tdborder">&nbsp;</td>
+                </tr>
+              </tbody>
+            </table></td>
+          </tr>
+          <tr>
+            <td style="text-align: center; font-size: 12px;"><table width="81%" border="0" cellspacing="0" cellpadding="0">
+              <tbody>
+                <tr>
+                  <td height="40" align="center"><em>** Borang ini hendaklah dikemukakan sekurang-kurangnya sebulan dari tarikh di atas</em></td>
+                  </tr>
+              </tbody>
+            </table></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td>Saya turut mengesahkan bahawa;</td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td><table width="100%" border="0" cellspacing="0" cellpadding="0" align="left">
+              <tr>
+                <td width="20%" valign="top"><table width="41%" border="0" align="center" cellpadding="0" cellspacing="0">
+                  <tbody>
+                    <tr>
+                      <td height="30" class="checkbox">&nbsp;</td>
+                      </tr>
+                    </tbody>
+                  </table></td>
+                <td width="80%" valign="top"><p>Saya tiada sebarang tunggakkan bayaran dengan institut; dan</p></td>
+                </tr>
+              <tbody>
+                </tbody>
+              </table></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td><table width="100%" border="0" cellspacing="0" cellpadding="0" align="left">
+              <tr>
+                <td width="17%" valign="top"><table width="41%" border="0" align="center" cellpadding="0" cellspacing="0">
+                  <tbody>
+                    <tr>
+                      <td height="30" class="checkbox">&nbsp;</td>
+                    </tr>
+                  </tbody>
+                </table></td>
+                <td width="83%" valign="top"><p>Saya akan memastikan akaun utiliti di atas nama saya akan ditutup pada hari</p>
+                  <p>saya keluar kuarters.</p></td>
+              </tr>
+              <tbody>
+              </tbody>
+            </table></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td align="justify">Saya bersedia untuk berbincang dengan Penyelia Kuarters bagi penetapan tarikh lawatan ke unit kuarters saya untuk pemeriksaan keadaan semasa kuarters sebelum pengeluaran Sijil Akuan Keluar Rumah.</td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td>Pengesahan Penghuni :</td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td><table width="100%" border="0" cellspacing="0" cellpadding="4">
+              <tbody>
+                <tr>
+                  <td width="40%" rowspan="4" valign="top" class="tdborder" ><em>Tandatangan</em> :</td>
+                  <td width="70%">&nbsp;</td>
+                </tr>
+                <tr>
+                  <td>&nbsp;&nbsp;Nama : _______________________________________________</td>
+                </tr>
+                <tr>
+                  <td>&nbsp;</td>
+                </tr>
+                <tr>
+                  <td>&nbsp;&nbsp;Tarikh : ______________________________________________</td>
+                </tr>
+              </tbody>
+            </table></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td><table width="100%" border="0" cellspacing="0" cellpadding="4" class="tdborder">
+              <tbody>
+                <tr>
+                  <td colspan="3"><strong>Pengesahan Penyelia Kuarters:</span></strong></td>
+                  <td width="40%" rowspan="4" valign="top" class="tdborder"><p>&nbsp;Tandatangan, Nama &amp; Tarikh:
+                  <tr>
+                  <td width="25%">Tarikh Diterima</td>
+                  <td width="4%">:</td>
+                  <td><table width="88%" border="0" cellspacing="0" cellpadding="0">
+                    <tbody>
+                      <tr>
+                        <td class="tdborder">&nbsp;</td>
+                      </tr>
+                    </tbody>
+                  </table></td>
+                  </tr>
+                <tr>
+                  <td width="25%">Tarikh Lawatan</td>
+                  <td width="4%" >:</td>
+                  td>
+                  <td width="31%"><table width="88%" border="0" cellspacing="0" cellpadding="0">
+                    <tbody>
+                      <tr>
+                        <td class="tdborder">&nbsp;</td>
+                      </tr>
+                    </tbody>
+                  </table></td>
+                  </tr>
+                <tr>
+                  <td width="25%"><p>No. Rujukan Diberikan</p>
+                    <p>&nbsp;</p></td>
+                  <td width="4%"><p>:</p>
+                    <p>&nbsp;</p></td>
+                  <td><table width="88%" border="0" cellspacing="0" cellpadding="0">
+                    <tbody>
+                      <tr>
+                        <td class="tdborder">&nbsp;</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                    <p>&nbsp;</p></td>
+                </tr>
+                </tbody>
+              </table></td>
+          </tr>
+        </tbody>
+      </table></td>
+    </tr>
+  </tbody>
+</table>
+	
+</body>
+</html>
