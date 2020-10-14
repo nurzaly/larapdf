f69e82c30db1c186ceb5f55122a8469e4ddadd23
unknown <mohdkairulnizam@gmail.com>
2020-10-14 10:37:55 +0800

    daftar pengisian - senarai keseluruhan

---
+++ resources/views/daftar-pengisian2.blade.php
@@ -0,0 +1,273 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Daftar Pengisian Kuarters (Senarai Keseluruhan)</title>
+<link rel="stylesheet" href="pdf.css">
+</head>
+
+<body><table width="100%" border="0" cellspacing="0" cellpadding="0" align="center">
+  <tbody>
+    <tr>
+      <td colspan="13"  align="right" style="font-size: 12px">BRG/KRTRS – PENGISIAN KUARTERS</td>
+    </tr>
+    <tr>
+      <td height="37" colspan="13" align="center" bgcolor="#ADADAD" class="tdborder" scope="col"><p><strong>DAFTAR PENGISIAN KUARTERS</strong></p>
+      <p><strong>(SENARAI KESELURUHAN)</strong><strong></strong></p></td>
+    </tr>
+    <tr>
+      <td height="37" colspan="13" scope="col" align="left">&nbsp;</td>
+    </tr>
+    <tr>
+      <th width="3%" rowspan="2" align="center" bgcolor="#ADADAD" class="tdborder" scope="col">Bil.</th>
+      <th colspan="5" align="center" bgcolor="#ADADAD" class="tdborder" scope="col">Kod Kuarters</th>
+      <th width="17%" rowspan="2" align="center" bgcolor="#ADADAD" class="tdborder" scope="col">Nama Penghuni/Pengguna</th>
+      <th width="13%" rowspan="2" align="center" bgcolor="#ADADAD" class="tdborder" scope="col">Gred Jawatan</th>
+      <th width="8%" rowspan="2" align="center" bgcolor="#ADADAD" class="tdborder" scope="col"><p>Tarikh Masuk</p>
+      <p>(Diisi sekiranya Kod Status=F)</p></th>
+      <th width="8%" rowspan="2" align="center" bgcolor="#ADADAD" class="tdborder" scope="col">Tarikh Mula Kosong (Diisi sekiranya Kod Status=K atau KR)</th>
+      <th width="8%" rowspan="2" align="center" bgcolor="#ADADAD" class="tdborder" scope="col">Tarikh Tamat Lantikan Tugas Yang Diperuntukkan Rumah Khas (Diisi sekiranya Rumah Khas)</th>
+      <th width="7%" rowspan="2" align="center" bgcolor="#ADADAD" class="tdborder" scope="col">Catatan Kerosakan (Diisi sekiranya Kod Status=KR)</th>
+      <th width="11%" rowspan="2" align="center" bgcolor="#ADADAD" class="tdborder" scope="col">Fungsi Yang Ditukar (Diisi sekiranya Kuarters di tukar fungsi)</th>
+    </tr>
+    <tr>
+      <th width="5%" align="center" bgcolor="#ADADAD" class="tdborder" scope="col">No. Unit Kuarters<strong></strong></th>
+      <th width="6%" align="center" bgcolor="#ADADAD" class="tdborder" scope="col">Jenis / kedudukan<strong></strong></th>
+      <th width="4%" align="center" bgcolor="#ADADAD" class="tdborder" scope="col">Kelas</th>
+      <th width="6%" align="center" bgcolor="#ADADAD" class="tdborder" scope="col">Tugas (sekiranya Rumah Khas)</th>
+      <th width="4%" align="center" bgcolor="#ADADAD" class="tdborder" scope="col">Status</th>
+    </tr>
+    <tr>
+      <td height="37" align="center" class="tdborder">
+      <td class="tdborder">&nbsp;</td>
+      <td align="center" class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+    </tr>
+    <tr>
+      <td height="37" align="center" class="tdborder">
+      <td class="tdborder">&nbsp;</td>
+      <td align="center" class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+    </tr>
+    <tr>
+      <td height="37" align="center" class="tdborder">
+      <td class="tdborder">&nbsp;</td>
+      <td align="center" class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+    </tr>
+    <tr>
+      <td height="37" align="center" class="tdborder">
+      <td class="tdborder">&nbsp;</td>
+      <td align="center" class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+    </tr>
+    <tr>
+      <td height="37" align="center" class="tdborder">    
+      <td class="tdborder">&nbsp;</td>
+      <td align="center" class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+    </tr>
+    <tr>
+      <td height="37" align="center" class="tdborder">    
+      <td class="tdborder">&nbsp;</td>
+      <td align="center" class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+    </tr>
+    <tr>
+      <td height="37" align="center" class="tdborder">    
+      <td class="tdborder">&nbsp;</td>
+      <td align="center" class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+    </tr>
+    <tr>
+      <td height="37" align="center" class="tdborder">    
+      <td class="tdborder">&nbsp;</td>
+      <td align="center" class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+    </tr>
+    <tr>
+      <td height="37" align="center" class="tdborder">    
+      <td class="tdborder">&nbsp;</td>
+      <td align="center" class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+    </tr>
+    <tr>
+      <td height="37" align="center" class="tdborder">    
+      <td class="tdborder">&nbsp;</td>
+      <td align="center" class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+    </tr>
+    <tr>
+      <td height="37" align="center" class="tdborder">    
+      <td class="tdborder">&nbsp;</td>
+      <td align="center" class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+    </tr>
+    <tr>
+      <td height="37" align="center" class="tdborder">    
+      <td class="tdborder">&nbsp;</td>
+      <td align="center" class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+    </tr>
+    <tr>
+      <td height="37" align="center" class="tdborder">    
+      <td class="tdborder">&nbsp;</td>
+      <td align="center" class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+    </tr>
+    <tr>
+      <td height="37" align="center" class="tdborder">    
+      <td class="tdborder">&nbsp;</td>
+      <td align="center" class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+    </tr>
+    <tr>
+      <td height="37" align="center" class="tdborder">
+      <td class="tdborder">&nbsp;</td>
+      <td align="center" class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+      <td class="tdborder">&nbsp;</td>
+    </tr>
+  </tbody>
+</table>
+<p><em style="font-size: 12px">** Sila tambah helaian lain sekiranya baris tidak mencukupi</em></p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+</body>
+</html>
